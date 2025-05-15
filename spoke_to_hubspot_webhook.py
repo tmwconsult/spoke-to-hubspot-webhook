f9a543edc5f236692ee6b2b810016131d7145d56
tmwconsult <tmw@tmwconsult.com>
2025-05-15 13:53:22 -0400

    Update spoke_to_hubspot_webhook.py

--- spoke_to_hubspot_webhook.py
+++ spoke_to_hubspot_webhook.py
@@ -94,7 +94,8 @@ def handle_inbound_sms():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port=int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 '''
 
 # Save to file
